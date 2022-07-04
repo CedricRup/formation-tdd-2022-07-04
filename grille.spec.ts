d6113f6938aec78aebbdfa16be0d2d2c5c2f52a5
BKsensei <rebecca.kaci@gmail.com>
2022-07-04 11:58:36 +0200

    second test a rouge pour le momemt

--- grille.spec.ts
+++ grille.spec.ts
@@ -1,23 +1,31 @@
 enum EtatCase {
-  Rouge
+  Rouge,
+  Jaune
 }
 
 class Grille {
 
-  getEtatCase(number: number, number2: number) {
-    return undefined;
+  getEtatCase(x: number, y: number) {
+    return EtatCase.Rouge;
   }
 
-  ajouterPion(number: number, Rouge: EtatCase) {
+  ajouterPion(colonne: number, couleur: EtatCase) {
     
   }
 }
 
 describe("grille", () => {
-  it("un pion dans la premiere collone", () => {
+  it("Quand j'ajoute un pion dans la premiere colonne, il tombe au fond de la colonne", () => {
     const grille = new Grille()
     grille.ajouterPion(0, EtatCase.Rouge)
     const etatCase = grille.getEtatCase(0,0)
     expect(etatCase).toBe(EtatCase.Rouge)
+  })
+  it("Si j'ajoute deux pions, ils s'empilent", () => {
+    const grille = new Grille()
+    grille.ajouterPion(0, EtatCase.Rouge)
+    grille.ajouterPion(0, EtatCase.Jaune)
+    const etatCase = grille.getEtatCase(0,1)
+    expect(etatCase).toBe(EtatCase.Jaune)
   })
 })
