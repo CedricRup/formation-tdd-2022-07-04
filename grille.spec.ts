ddd264229a61428192a8797ce5f9f34629534e45
Adrien GOGOIS <ad.gogois@gmail.com>
2022-07-04 14:27:40 +0200

    Etat case vide

--- grille.spec.ts
+++ grille.spec.ts
@@ -1,16 +1,19 @@
 enum EtatCase {
   Rouge,
-  Jaune
+  Jaune,
+  Vide
 }
 
 class Grille {
 
   getEtatCase(x: number, y: number) {
-    return EtatCase.Rouge;
+    return this.colonne1[y];
   }
 
+  colonne1 : EtatCase[] = [];
+
   ajouterPion(colonne: number, couleur: EtatCase) {
-    
+    this.colonne1.push(couleur);
   }
 }
 
@@ -21,11 +24,26 @@ describe("grille", () => {
     const etatCase = grille.getEtatCase(0,0)
     expect(etatCase).toBe(EtatCase.Rouge)
   })
+
   it("Si j'ajoute deux pions, ils s'empilent", () => {
     const grille = new Grille()
     grille.ajouterPion(0, EtatCase.Rouge)
     grille.ajouterPion(0, EtatCase.Jaune)
     const etatCase = grille.getEtatCase(0,1)
     expect(etatCase).toBe(EtatCase.Jaune)
+  })
+
+  it("Si j'ajoute deux pions de même couleur, ils s'empilent", () => {
+    const grille = new Grille()
+    grille.ajouterPion(0, EtatCase.Rouge)
+    grille.ajouterPion(0, EtatCase.Rouge)
+    const etatCase = grille.getEtatCase(0,1)
+    expect(etatCase).toBe(EtatCase.Rouge)
+  })  
+
+  it("Quand je veux l'état d'une case sans ajouter de pion -> Etat vide", () => {
+    const grille = new Grille();
+    const etatCase = grille.getEtatCase(0, 0);
+    expect(etatCase).toBe(EtatCase.Vide);
   })
 })
