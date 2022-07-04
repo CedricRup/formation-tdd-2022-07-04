a11cce12800d5cc2c6cf965d7c2e606d2ce0fa9d
Hurtsich <justin.boudt@gmail.com>
2022-07-04 14:49:58 +0200

    Ajout Vide

--- grille.spec.ts
+++ grille.spec.ts
@@ -4,46 +4,57 @@ enum EtatCase {
   Vide
 }
 
+enum CouleurPion {
+  Rouge,
+  Jaune,
+}
+
 class Grille {
 
+  colonne1 : EtatCase[] = []
+  
   getEtatCase(x: number, y: number) {
-    return this.colonne1[y];
+    const etatCase = this.colonne1[y]
+    if (etatCase != EtatCase.Jaune && etatCase != EtatCase.Rouge)
+    {
+      return EtatCase.Vide
+    }
+
+    return etatCase
   }
 
-  colonne1 : EtatCase[] = [];
-
-  ajouterPion(colonne: number, couleur: EtatCase) {
-    this.colonne1.push(couleur);
+  ajouterPion(colonne: number, couleur: CouleurPion) {
+    this.colonne1.push(couleur === CouleurPion.Jaune ? EtatCase.Jaune : EtatCase.Rouge)
   }
 }
 
 describe("grille", () => {
   it("Quand j'ajoute un pion dans la premiere colonne, il tombe au fond de la colonne", () => {
     const grille = new Grille()
-    grille.ajouterPion(0, EtatCase.Rouge)
+    grille.ajouterPion(0, CouleurPion.Rouge)
     const etatCase = grille.getEtatCase(0,0)
-    expect(etatCase).toBe(EtatCase.Rouge)
+    expect(etatCase).toBe(CouleurPion.Rouge)
   })
 
   it("Si j'ajoute deux pions, ils s'empilent", () => {
     const grille = new Grille()
-    grille.ajouterPion(0, EtatCase.Rouge)
-    grille.ajouterPion(0, EtatCase.Jaune)
+    grille.ajouterPion(0, CouleurPion.Rouge)
+    grille.ajouterPion(0, CouleurPion.Jaune)
     const etatCase = grille.getEtatCase(0,1)
-    expect(etatCase).toBe(EtatCase.Jaune)
+    expect(etatCase).toBe(CouleurPion.Jaune)
   })
 
   it("Si j'ajoute deux pions de même couleur, ils s'empilent", () => {
     const grille = new Grille()
-    grille.ajouterPion(0, EtatCase.Rouge)
-    grille.ajouterPion(0, EtatCase.Rouge)
+    grille.ajouterPion(0, CouleurPion.Rouge)
+    grille.ajouterPion(0, CouleurPion.Rouge)
     const etatCase = grille.getEtatCase(0,1)
-    expect(etatCase).toBe(EtatCase.Rouge)
+    expect(etatCase).toBe(CouleurPion.Rouge)
   })  
 
-  it("Quand je veux l'état d'une case sans ajouter de pion -> Etat vide", () => {
+  it("Quand je demande l'état d'une case vide le resultat est vide", () => {
     const grille = new Grille();
-    const etatCase = grille.getEtatCase(0, 0);
-    expect(etatCase).toBe(EtatCase.Vide);
+    const etatCase = grille.getEtatCase(0, 0)
+    expect(etatCase).toBe(EtatCase.Vide)
   })
 })
