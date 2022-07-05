30c5325505e3a534e5bcd6825e95514efe494888
dadadorly <david.nicola.luu@gmail.com>
2022-07-05 11:29:08 +0200

    Test joueur rejoue

--- arbitre.spec.ts
+++ arbitre.spec.ts
@@ -19,11 +19,17 @@ class Arbitre {
   }
 
   jouerPion(colonne: number) {
-    this.grille.ajouterPion(colonne,CouleurPion.Jaune)
-    if (this.joueurCourant === Joueur.Rouge){
-      this.joueurCourant = Joueur.Jaune
-    }else{
-      this.joueurCourant = Joueur.Rouge
+    try {
+      if (this.joueurCourant === Joueur.Rouge) {
+        this.grille.ajouterPion(colonne, CouleurPion.Rouge)
+        this.joueurCourant = Joueur.Jaune
+      } else {
+        this.grille.ajouterPion(colonne, CouleurPion.Jaune)
+        this.joueurCourant = Joueur.Rouge
+      }
+    }
+    catch(erreur : unknown){
+      throw erreur
     }
   }
 }
@@ -51,12 +57,32 @@ describe("arbitre", () => {
     expect(arbitre.getJoueurCourant()).toBe(Joueur.Jaune)
   })
 
-  it("Quand on joue, on met un pion dans la grille", () => {
+  it("Quand Jaune joue, on met un pion jaune dans la grille", () => {
     arbitre.jouerPion(0)
     const etatCase = grille.getEtatCase(0,0)
     expect(etatCase).toBe(EtatCase.Jaune)
   })
 
+  it("Quand Rouge joue on met un pion rouge dans la grille", () => {
+    arbitre.jouerPion(0)
+    arbitre.jouerPion(0)
+    const etatCase = grille.getEtatCase(0,1)
+    expect(etatCase).toBe(EtatCase.Rouge)
+  })
+
+  it("Quand un joueur joue un coup interdit, il rejoue", () => {
+    try{
+    arbitre.jouerPion(7)
+    }
+    catch(erreur: unknown) {
+
+    }
+    expect(arbitre.getJoueurCourant()).toBe(Joueur.Jaune)
+  })
+
+  it("Quand un joueur joue un coup interdit, une erreur est remontée", () => {
+    expect(() => arbitre.jouerPion(7)).toThrow("Cette colonne n'existe pas")
+  })
 
 
 
