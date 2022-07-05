7b7337363952af35767a4ecfd57db93be3337112
Bullfrog <syczjason@gmail.com>
2022-07-05 15:35:43 +0200

    Jasn 3 prompt

--- jeu.spec.ts
+++ jeu.spec.ts
@@ -24,4 +24,11 @@ describe("jeu", () =>{
         // expect(fausseConsole.aEteAppeleeAvec("C'est à jaune de jouer")).toBe(true)
         expect(fausseConsole.ecrire).toHaveBeenCalledWith("C'est à jaune de jouer")
     })
+
+    it("Quand un joueur joue un coup valide, la console envoie un message de validation", () => {
+        const fausseConsole = {ecrire : jest.fn()}
+        const jeu = new Jeu(fausseConsole)
+        jeu.demarrerPartie()
+        expect(fausseConsole.ecrire).toHaveBeenCalledWith("Coup valide")
+    })
 })
--- jeu.ts
+++ jeu.ts
@@ -1,16 +1,46 @@
+
+
+import {Arbitre} from "./arbitre";
+import {Grille} from "./grille";
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
 export interface IConsole {
     ecrire : (message : string) => void
 }
 
+interface IPrompt {
+    demanderColonne : ((colonne:number) => void)
+}
+
+class VraiPrompt implements IPrompt{
+    demanderColonne(){
+        readline.question("à toi de jouer", (colonne:string)=> {
+            return parseInt(colonne)
+        })
+    }
+}
+
 export class Jeu {
     console : IConsole
+    arbitre : Arbitre
 
     constructor(console : IConsole){
         this.console = console
+        this.arbitre = new Arbitre(new Grille())
     }
 
     demarrerPartie() {
         this.console.ecrire("C'est à jaune de jouer")
+        readline.question("à toi de jouer", (colonne:string)=> {
+            if (colonne) {
+                this.arbitre.jouerPion(parseInt(colonne))
+                this.console.ecrire("Coup valide")
+            }
+        })
     }
 }
 
